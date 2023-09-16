4384cb952bb4ca230052e6af93fc08e73e734be5
ArrySawyer <102818765+ArrySawyer@users.noreply.github.com>
2023-09-16 12:38:21 +0100

    Fixed the bug 😎

--- app/page.tsx
+++ app/page.tsx
@@ -160,21 +160,6 @@ export default function HomePage() {
           <PokemonTable />
         </div>
       </section>
-      <div className="h-screen bg-blue-600">
-        <p>pokemon </p>
-      </div>
-      <div className="h-screen bg-red-600">
-        <p>pokemon </p>
-      </div>
-      <div className="h-screen bg-green-600">
-        <p>pokemon </p>
-      </div>
-      <div className="h-screen bg-pink-600">
-        <p>pokemon </p>
-      </div>
-      <div className="h-screen bg-yellow-600">
-        <p>pokemon </p>
-      </div>
     </div>
   );
 }
