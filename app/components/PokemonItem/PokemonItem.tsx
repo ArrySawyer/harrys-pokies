16df1de4e3ae2ac495f47e78c2385c07313b2b05
ArrySawyer <102818765+ArrySawyer@users.noreply.github.com>
2023-09-16 12:19:49 +0100

    Removed blur on PokemonItem

--- app/components/PokemonItem/PokemonItem.tsx
+++ app/components/PokemonItem/PokemonItem.tsx
@@ -137,18 +137,20 @@ const PokemonItem = ({ pokemonDetails }: PokemonItemProps) => {
 
   return (
     <div className="h-80">
-      <div className="group relative flex justify-center">
+      <div className="group relative flex justify-center transition-all ease-out duration-300">
         <Image
           width="256"
           height="256"
-          className="absolute -top-40 w-64 z-10"
+          className="absolute -top-40 w-64 z-10 origin-bottom sm:group-hover:scale-110 transition-all ease-out duration-300"
           src={`${pokemonDetails.image}`}
           alt={`${pokemonDetails.name}`}
         />
         <div
-          className={`${mainTypeStyle.wrapper} bg-[#233349] text-white relative w-full h-[19.3rem] sm:h-[17rem] flex flex-col items-center border-t rounded-2xl overflow-hidden`}
+          className={`${mainTypeStyle.wrapper} bg-[#233349] text-white relative w-full h-[19.3rem] sm:h-[17rem] sm:group-hover:h-[19.3rem] flex flex-col items-center border-t rounded-2xl overflow-hidden shadow-2xl sm:shadow-lg group-hover:shadow-2xl sm:group-hover:scale-105 transition-all ease-out duration-300`}
         >
-          <div className={`w-32 h-32 ${mainTypeStyle.main}`}></div>
+          <div
+            className={`w-32 h-32 ${mainTypeStyle.main} group-hover:scale-125 transition-all ease-out duration-300`}
+          ></div>
           <div className="z-10 flex flex-col items-center">
             <p className="text-xl font-bold">#{pokemonDetails.id}</p>
             <p className="text-3xl font-bold mb-3 capitalize">
@@ -185,7 +187,7 @@ const PokemonItem = ({ pokemonDetails }: PokemonItemProps) => {
           </div>
           <div className="absolute bottom-0 w-full px-8">
             <div
-              className={`${mainTypeStyle.button} w-fit mx-auto flex items-center gap-1 pl-3 pr-2 py-1 border-2 border-white rounded-full sm:opacity-0 relative z-20 `}
+              className={`${mainTypeStyle.button} w-fit mx-auto flex items-center gap-1 pl-3 pr-2 py-1 border-2 border-white rounded-full sm:opacity-0 group-hover:opacity-100 transition-opacity ease-in-out duration-75 relative z-20 hover:bg-white`}
             >
               <Link
                 href={`/pokemon/${pokemonDetails.name}`}
@@ -209,7 +211,9 @@ const PokemonItem = ({ pokemonDetails }: PokemonItemProps) => {
                 </svg>
               </Link>
             </div>
-            <div className={`h-5 w-full ${mainTypeStyle.main}`}></div>
+            <div
+              className={`h-5 w-full ${mainTypeStyle.main} scale-[6] translate-y-6`}
+            ></div>
           </div>
         </div>
       </div>
